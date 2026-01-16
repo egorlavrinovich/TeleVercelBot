71dedbc4dc3b74ccf54428c4fdca3cedac96402c
Egorlaptop <egorlavrinovich@gmail.com>
2026-01-16 19:08:54 +0300

    Фикс бага

--- db.mjs
+++ db.mjs
@@ -8,7 +8,7 @@ const client = new MongoClient(process.env.DB_TOKEN, {
   },
 });
 
-export async function run() {
+export default async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
