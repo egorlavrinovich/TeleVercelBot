06c5a9f17435136c8e57b96a97eec80b226703e3
Egorlaptop <egorlavrinovich@gmail.com>
2026-01-16 18:21:20 +0300

    Базовая структура

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,7 @@
-import TeleBot from "telebot"
+import TeleBot from "telebot";
 
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.on("text", msg => msg.reply.text(msg.text))
+bot.on("text", (msg) => msg.reply.text(msg.text));
 
-export default bot
+export default bot;
